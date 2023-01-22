fb7ef9cc09d110546983b729927e4c3c4779d387
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-01-22 12:50:50 -0600

    Added the arrow up key to jump over obstacles.

---
+++ script.js
@@ -0,0 +1,37 @@
+let character = document.getElementById('character')
+
+let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue
+('bottom'));
+
+let characterRight = parseInt(window.getComputedStyle(character).getPropertyValue
+('right'));
+
+let characterWidth = parseInt(window.getComputedStyle(character).getPropertyValue
+('width'));
+
+let ground = document.getElementById('ground');
+
+let groundBottom = parseInt(window.getComputedStyle(ground).getPropertyValue('bottom'))
+
+let groundHeight = parseInt(window.getComputedStyle(ground).getPropertyValue('height'))
+
+let isJumping = false;
+let upTime;
+let downTime;
+
+function jump(){
+    if(isJumping) return;
+    upTime = setInterval(() => {
+        characterBottom += 10;
+        character.style.bottom = characterBottom + 'px';
+        isJumping = true;
+    },20);
+}
+
+function control(e){
+    if (e.key === 'ArrowUp' || e.key == ''){
+        jump();
+    }
+}
+
+document.addEventListener('keydown', control)
